379931872185e00842215da6f3193249ed9bc745
Learnuel Linus <learnueltech@gmail.com>
2023-02-26 15:41:03 +0100

    fix: update logger config

--- src/controllers/user.constroller.js
+++ src/controllers/user.constroller.js
@@ -45,3 +45,44 @@ exports.defaultAccount = async (req, res, next) => {
     next(error);
   }
 }
+
+exports.createAccount = async (req, res, next) => {
+  try{
+    if(!req.role) return next(APIError.unauthenticated());
+    if(req.role !== CONSTANTS.USER_TYPES[3]) return next(APIError.unauthorized());
+    const {firstName, lastName, telephone, email, password, role} = req.body;
+    if(!firstName) return next(APIError.badRequest("Firstname is required"));
+    if(!lastName) return next(APIError.badRequest("Lasttname is required"));
+    if(!telephone) return next(APIError.badRequest("Telephone number is required"));
+    if(!email) return next(APIError.badRequest("Email is required"));
+    if(!password) return next(APIError.badRequest("Password is required"));
+    //validate email
+    if(!isValidEmail(email)) return next(APIError.badRequest(`${ERROR_FIELD.INVALID_EMAIL}`));
+    //check if email already exist for am employee
+    const mailExist = await EmployeeModel.findOne({email}).exec();
+    if(mailExist) return next(APIError.badRequest(`${email} is not available`));
+    //checke if default supper accound already exist
+    const superAccount = await AccountModel.findOne({role:CONSTANTS.USER_TYPES[3]});
+    if(superAccount) return next(APIError.customError("Default account already exist", 403));
+    //encrypt password
+    const hashedPasword = hashSync(password,10);
+    //create employee
+    const employee = await EmployeeModel.create({
+      firstName,
+      lastName,
+      telephone,
+      email,
+    });
+    //create account
+    await AccountModel.create({
+      username: email,
+      password: hashedPasword,
+      employee: employee._id,
+      role: CONSTANTS.USER_TYPES[3]
+    });
+    logger.info("Default account created successfully", {meta: "account-service"})
+    res.status(201).json({success: true, msg: "Account created successfully"});
+  }catch(error) {
+    next(error);
+  }
+}
